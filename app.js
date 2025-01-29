96903bf55dfbd34866aee13956e35de1546b58a1
Zebastian Rodriguez <Zebo5914@gmail.com>
2025-01-28 20:18:54 -0500

    finished product

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ dotenv.config();
 const mongoose = require("mongoose");
 const prompt = require("prompt-sync")();
 const username = prompt("What is your name? ");
-const Customer = require('./models/customer');
+const Customer = require('./models/customer.js');
 
 
 const main = async () => {
@@ -14,7 +14,7 @@ const main = async () => {
 
 	const disconnect = async () => {
 		await mongoose.disconnect();
-		console.log("Goodbye!");
+		console.log("exiting...");
 		process.exit();
 	};
 
@@ -38,8 +38,8 @@ const main = async () => {
     };
 
     const createCustomer = async () => {
-       const name = prompt('What is the New Customer Name?');
-       const age = prompt('How Old is the new Customer?');
+       const name = prompt('What is the New Customer Name? ');
+       const age = prompt('How Old is the new Customer? ');
 
        const customer = await Customer.create({
         name,
@@ -50,14 +50,40 @@ const main = async () => {
 
     const viewCustomers = async () => {
         const customers = await Customer.find({});
-        console.log('All customers:', customers);
-    }
+        console.log("Below is a list of your customers: ");
+        customers.forEach((customer) => {
+            console.log(`id:--${customer.id}, Name: ${customer.name}, Age: ${customer.age}`);
+        })
+    };
+
+    const updateCustomer = async () => {
+        console.log('Below is a list of your customers: ');
+        await viewCustomers();
+        const id = prompt('Copy and paste the id of the customer you would like to update here:');
+        console.log('You selected:', id);
+        const name = prompt("What is the customers new name? ");
+        const age = prompt("What is the customers new age? ");
+
+        const customer = await Customer.findByIdAndUpdate(id , {name, age}, {new: true});
+        console.log("Updated user:", customer);
+    };
+
+    const deleteCustomer = async () => {
+        console.log('Here is a list of your customers');
+        await viewCustomers();
+        const id = prompt('Copy and paste the id of the customer you would like to update here:');
+        console.log('You selected:', id);
+        const deletedCustomer = await Customer.findByIdAndDelete(id);
+        console.log("Deleted customer:", deletedCustomer);
+    };
 
     while (true) {
     const answer = welcome();
-    if (answer === '5') await disconnect();
     if (answer === '1') await createCustomer();
     if (answer === '2') await viewCustomers();
+    if (answer === '3') await updateCustomer();
+    if (answer === '4') await deleteCustomer();
+    if (answer === '5') await disconnect();
     };
 };
 
